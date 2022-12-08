106ed3d42d6f0cc8e60711ac944c1f713445c667
Tof <tof.aliev@ru.axept.com>
2022-12-08 19:58:03 +0600

    Added healthchecks, refs #5

--- src/web/routers/api.py
+++ src/web/routers/api.py
@@ -1,6 +1,17 @@
 from src.web.app import app
 
 
-@app.route('/hello_world', methods=["GET"])
-def hello_world():
-    return 'Hello, World!'
+@app.route('/health/readiness', methods=["GET"])
+def healthcheck():
+    """
+    Текущее состояние готовности сервиса
+    """
+    return 200
+
+
+@app.route('/health/liveness', methods=["GET"])
+def liveness():
+    """
+    Возвращает информацию о работоспособности сервиса
+    """
+    return 200
