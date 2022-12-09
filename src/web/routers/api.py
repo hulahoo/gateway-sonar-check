ade7d27f4994b9704ca8e2bf489b3c7958fe83ac
Tof <tof.aliev@ru.axept.com>
2022-12-09 21:11:07 +0600

    Added test logging, refs #5

--- src/web/routers/api.py
+++ src/web/routers/api.py
@@ -1,17 +1,23 @@
+from loguru import logger
+
 from src.web.app import app
 
 
 @app.route('/health/readiness', methods=["GET"])
+@logger.catch
 def healthcheck():
     """
     Текущее состояние готовности сервиса
     """
+    logger.info("Health checking started")
     return 200
 
 
 @app.route('/health/liveness', methods=["GET"])
+@logger.catch
 def liveness():
     """
     Возвращает информацию о работоспособности сервиса
     """
+    logger.info("Health checking started")
     return 200
