6c71c9d0cd4605d5c6a095957d5aff3d5efb0c7d
Tof <tof.aliev@ru.axept.com>
2023-01-13 15:34:30 +0600

    Added indicator_id column to stat_received_objects table, refs #5

--- src/events_gateway/models/models.py
+++ src/events_gateway/models/models.py
@@ -1,5 +1,10 @@
 from events_gateway.models.abstract import IDBase, TimestampBase
 
+from sqlalchemy import Column
+from sqlalchemy.dialects.postgresql import UUID
+
 
 class StatReceivedObjects(IDBase, TimestampBase):
     __tablename__ = "stat_received_objects"
+
+    indicator_id = Column(UUID(as_uuid=True), nullable=True)
